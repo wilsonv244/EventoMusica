07c4a286346615291e1323c70e2694ca4bbb5a33
wilsonv244 <39635463+wilsonv244@users.noreply.github.com>
2021-06-03 17:58:44 -0500

    Subiendo files a la pagina

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 
 const{series, parallel, src, dest, watch} = require('gulp');
 const sass = require('gulp-sass');
-
+const imagenmin = require('gulp-imagemin');
+const notify = require('gulp-notify');
+const webp = require('gulp-webp');
 //funciones//
-
+const paths = {
+    imagenes = 'src/img/**/*',
+}
 function css( ){
     return src('src/scss/app.scss')
         .pipe( sass())
@@ -16,6 +20,18 @@ function minificar( ){
         }))
         .pipe( dest ('./build/css'))
 }
+function imagenes(){
+    return src(paths.imagenes)
+        .pipe(imagenmin ())
+        .pipe( dest('./build/img'))
+        .pipe( notify({message: 'Imagen Minificada'}));
+}
+function versionwebp(){
+    return src(paths.imagenes)
+    .pipe(webp ( ))
+    .pipe(dest ('./build/img'))
+    .pipe( notify ({ message: 'version Weblista'}));
+}
 function watcharchivos(){
     watch( 'src/scss/**/*.scss', css); //*= la carpeta actual **= todos los archivos
 }
@@ -23,6 +39,9 @@ function watcharchivos(){
 exports.css = css;
 exports.minificar = minificar;
 exports.watcharchivos= watcharchivos;
+exports.imagenes = imagenes;
+exports.versionwebp = versionwebp;
+exports.default = series( css, imagenes, watcharchivos, versionwebp);
 
 
 
